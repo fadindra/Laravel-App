6ae2158636839e9118c4b9b01d875eeaeab7c507
dipeshbanjara <fadindrarajbanjara.44@gmail.com>
2022-05-21 17:46:32 +0545

    HTML MINOR

--- resources/views/layout.blade.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<title>Hello</title>
-<link rel="stylesheet" href="/app.css">
-
-<body>
-  @yield('content')
-</body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,18 @@
-@extends('layout')
+<x-layout>
+    @include('post_header')
 
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach
-@endsection
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post" class="bg-red-500" />
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No Posts Yet. Please Check Back Later</p>
+        @endif
+    </main>
+</x-layout>
