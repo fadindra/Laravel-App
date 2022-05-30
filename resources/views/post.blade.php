7783e08fb766a184101e86452d0f332b4f90ebe7
dipeshbanjara <fadindrarajbanjara.44@gmail.com>
2022-05-30 14:42:14 +0545

    HTML MINOR

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,4 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     <article>
         <h1>
             {!! $post->title !!}
@@ -14,4 +12,4 @@
         </div>
     </article>
     <a href="/">Go Back</a>
-@endsection
+</x-layout>
